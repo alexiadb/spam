ad98843223676ca00b230c90b34ff4a56006f687
alexiadb <49482351+alexiadb@users.noreply.github.com>
2022-02-03 15:00:15 +0100

    4 commit

--- streamlitApp.py
+++ streamlitApp.py
@@ -9,11 +9,17 @@ Created on Thu Feb  3 09:27:13 2022
 import streamlit as st
 import pandas as pd
 
+
+
+df = pd.read_csv("https://github.com/alexiadb/spam/blob/main/spam.csv", encoding='latin-1').head()
+st.write(df)
+
+
+
+
 st.write("""#my first app
          Hello *world*
          """)
      
          
-df= pd.read_csv("spam.csv", encoding='latin-1')
-st.line_chart(df)
-#data.info()       
+
